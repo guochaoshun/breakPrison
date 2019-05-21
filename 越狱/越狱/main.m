325ddea507f89ffd26a9bbb51503ec9407105a09
guocs <guocs@zwy.com>
2019-05-21 18:19:00 +0800

    禁止了GDB调试

--- 越狱/越狱/main.m
+++ 越狱/越狱/main.m
@@ -9,7 +9,29 @@
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
+// 禁止GDB调试 ：https://blog.csdn.net/yiyaaixuexi/article/details/18222339
+#import <dlfcn.h>
+#import <sys/types.h>
+
+typedef int (*ptrace_ptr_t)(int _request, pid_t _pid, caddr_t _addr, int _data);
+#if !defined(PT_DENY_ATTACH)
+#define PT_DENY_ATTACH 31
+#endif  // !defined(PT_DENY_ATTACH)
+
+void disable_gdb() {
+    void* handle = dlopen(0, RTLD_GLOBAL | RTLD_NOW);
+    ptrace_ptr_t ptrace_ptr = dlsym(handle, "ptrace");
+    ptrace_ptr(PT_DENY_ATTACH, 0, 0, 0);
+    dlclose(handle);
+}
+
 int main(int argc, char * argv[]) {
+    
+    
+#ifndef DEBUG
+    disable_gdb();
+#endif
+    
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
